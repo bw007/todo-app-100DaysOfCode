3ecb12c8a4dc1a1ff7e02cc73b80ee45cf1b71a2
bw007 <abdulaziz00791@gmail.com>
2024-03-22 12:31:02 +0500

    Notification message and edit data

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ class Todo {
   collection = [];
   tags = [];
   menu = [];
+  data = [];
   constructor(collection, tags, menu, data) {
     this.collection = collection;
     this.tags = tags?.reverse();
@@ -11,12 +12,12 @@ class Todo {
     this.data = data;
   }
 
+  // Create sidebar menu
   createMenu() {
     const firstNav = document.querySelector("#first"),
       lastNav = document.querySelector("#last");
 
     this.data = JSON.parse(localStorage.getItem("data")) || [];
-    console.log(this.data);
     
     firstNav.innerHTML = "";
     this.menu.forEach(item => {
@@ -24,7 +25,7 @@ class Todo {
         <a class="sidebar__link" id="link" href="#${item}">
           ${item.split("-").join(" ")}
           <span class="count" id="daily">
-            ${ item == "today" 
+            ${ item == "today"
               ? this.data?.filter(f => convertDate(f.deadline) == convertDate(new Date())).length ?? 0 
               : this.data?.length ?? 0 
             }
@@ -51,8 +52,8 @@ class Todo {
     const home = document.querySelector(".sidebar__nav [href='#today']");
 
     logo.onclick = () => {
-      for (const i of links) {
-        i.classList.remove("sidebar__link--active");
+      for (const item of links) {
+        item.classList.remove("sidebar__link--active");
       }
       home.classList.add("sidebar__link--active");
       this.getStoredData(home.getAttribute("href"), this.data)
@@ -62,8 +63,8 @@ class Todo {
       el.classList.toggle("sidebar__link--active", el.getAttribute("href") === window.location.hash);
 
       el.onclick = (e) => {
-        for (const i of links) {
-          i.classList.remove("sidebar__link--active");
+        for (const item of links) {
+          item.classList.remove("sidebar__link--active");
         }
 
         el.classList.add("sidebar__link--active");
@@ -71,8 +72,222 @@ class Todo {
       }
     });
   }
+  // ------------------
 
-  createModal() {
+  // Notification
+  notifMessage(type) {
+    let cls = type == "error" ? "danger" : "success";
+    let msg = type == "new" ? "Added successfully!" 
+      : type == "edit" ? "Edited successfully" 
+      : type;
+
+    let notif = document.querySelector(".notif");
+    const notifText = document.createElement("p"),
+      icon = document.createElement("img"),
+      closeIcon = document.createElement("img");
+    icon.src = `./imgs/${type == "error" ? "danger" : "success"}.svg`;
+    closeIcon.src = "./imgs/close.svg";
+
+    if (!notif) {
+      notif = document.createElement("div");
+      notif.classList.add("notif");
+      document.body.append(notif);
+    }
+    
+    notif.innerHTML = "";
+    setTimeout(() => {
+      notif.classList.add(cls);
+    }, 150);
+    notifText.textContent = msg;
+
+    notif.append(icon, notifText, closeIcon);
+
+    const time = setTimeout(() => {
+      notif.classList.remove(cls);
+    }, 4000);
+
+    closeIcon.onclick = () => {
+      notif.classList.remove(cls);
+      clearTimeout(time);
+    }
+  }
+  // -----------
+
+  // Create modal
+  createModal(type, id) {
+    const main = document.querySelector(".main");
+    let modal = document.querySelector(".modal");
+    
+    if (modal) {
+      if (modal.classList.contains("modal__hidden")) {
+        modal.classList.remove("modal__hidden");
+      }
+    } else {
+      modal = document.createElement("div");
+      modal.classList.add("modal");    
+    }
+    modal.innerHTML = "";
+
+    let modalbox = document.createElement("div");
+      modalbox.classList.add("modal__box");
+
+      let modalTitle = document.createElement("h1");
+      modalTitle.classList.add("modal__title");
+      modalTitle.textContent = `${type} task`;
+
+      let modalForm = document.createElement("form");
+      modalForm.classList.add("modal__form");
+
+      this.collection.forEach(item => {
+        if (item.type == "select") {
+          let select = document.createElement("select");
+          select.classList.add("modal__select");
+          select.setAttribute("required", item.req);
+          select.setAttribute("name", item.name);
+          this.tags.forEach(opt => {
+            let option = document.createElement("option")
+            option.textContent = opt.split("-").join(" ");
+            option.value = opt;
+            select.appendChild(option);
+          });
+          
+          modalForm.appendChild(select);
+        } else {
+          let input = document.createElement("input");
+          input.classList.add("modal__input");
+          input.setAttribute("required", item.req);
+          input.setAttribute("type", item.type);
+          input.setAttribute("name", item.name);
+          input.placeholder = item.text;
+          modalForm.append(input);
+        }
+      });
+
+      let btnParent = document.createElement("div");
+      btnParent.classList.add("modal__btns");
+
+      let cancel = document.createElement("button");
+      cancel.classList.add("modal__btn", "modal__btn--danger");
+      cancel.textContent = "Cancel";
+      
+      let submit = document.createElement("button");
+      submit.setAttribute("type", "submit");
+      submit.classList.add("modal__btn", "modal__btn--success");
+      submit.textContent = type == "new" ? "create" : type;
+
+      btnParent.append(cancel, submit);
+      modalForm.appendChild(btnParent);
+      modalbox.append(modalTitle, modalForm);
+
+      modal.appendChild(modalbox);
+      main.appendChild(modal)
+
+      var inputs = document.querySelectorAll('.modal__input');
+      const storedData = JSON.parse(localStorage.getItem("data"));
+
+      if (id) {
+        const data = storedData.find(item => item.id == id);
+        for (const key in data) {
+          let inputElement = modalForm.querySelector(`[name="${key}"]`);
+            if (inputElement) {
+              inputElement.value = data[key];
+            }
+        }
+      }
+
+      modal.onclick = (e) => {
+        if (e.target.classList.contains("modal")) {
+          modal.classList.add("modal__hidden");
+          inputs.forEach(el => {
+            el.classList.remove("modal__input--danger")
+          });
+        }
+      }
+
+      cancel.onclick = (e) => {
+        e.preventDefault();
+        
+        modalForm.reset();
+
+        inputs.forEach(el => {
+          el.classList.remove("modal__input--danger")
+        });
+        
+        setTimeout(() => {
+          modal.classList.add("modal__hidden");
+        }, 100);
+      };
+
+      submit.onclick = (e) => {
+        e.preventDefault();
+        
+        let data = {};
+        let form = new FormData(modalForm);
+
+        form.forEach((value, name) => {
+          data.id = Date.now();
+          data[name] = value;
+          data.status = false;
+          data.createdTime = new Date();
+        });
+        
+        let inp = 0;
+        inputs.forEach(el => {
+          if (!el.value.trim()) {
+            el.classList.add("modal__input--danger")
+          } else {
+            inp++;
+            el.classList.remove("modal__input--danger")
+          }
+        });
+
+        if (inp == 3) {
+          modalForm.reset();
+          inputs.forEach(el => {
+            el.classList.remove("modal__input--danger")
+          });
+          modal.classList.add("modal__hidden");
+
+          if (type == "edit" && id) {
+            this.data = [ 
+              ...storedData.map(item => {
+                if (item.id != id) return item;
+                return { ...data, status: item.status, id: item.id };
+              })
+            ]
+          } else if (!id && storedData) {
+            this.data = [ { ...data }, ...storedData ];
+          } else {
+            this.data = [ { ...data } ];
+          }
+          localStorage.setItem("data", JSON.stringify(this.data));
+
+          const { hash } = window.location;
+          const currentDate = convertDate(new Date());
+
+          const filteredData = this.data.filter(item => {
+            switch (hash) {
+              case "#all":
+                return true;
+              case "#today":
+                return convertDate(item.deadline) === currentDate;
+              default:
+                return item.tag === hash.slice(1);
+            }
+          });
+
+          setTimeout(() => {
+            this.notifMessage(type);
+            this.createMenu();
+            this.renderStoredData(filteredData);
+          }, 100);
+        }
+      }
+  }
+  // ----------------
+
+  // Create modal and save data
+  addData() {
     const main = document.querySelector(".main");
     let addBtn = document.querySelector(".add");
 
@@ -80,199 +295,53 @@ class Todo {
       addBtn = document.createElement("button");
       addBtn.classList.add("add");
       addBtn.textContent = "+";
-      main.append(addBtn)
+      main.append(addBtn);
     }
 
     addBtn.onclick = (e) => {
-      let modal = document.querySelector(".modal");
-
-      if (modal) {
-        if (modal.classList.contains("modal__hidden")) {
-          modal.classList.remove("modal__hidden");
-        }
-      } else {
-        modal = document.createElement("div");
-        modal.classList.add("modal");
-
-        let modalbox = document.createElement("div");
-        modalbox.classList.add("modal__box");
-
-        let modalTitle = document.createElement("h1");
-        modalTitle.classList.add("modal__title");
-        modalTitle.textContent = "New task";
-
-        let modalForm = document.createElement("form");
-        modalForm.classList.add("modal__form");
-
-        this.collection.forEach(item => {
-          if (item.type == "select") {
-            let select = document.createElement("select");
-            select.classList.add("modal__select");
-            select.setAttribute("required", item.req);
-            select.setAttribute("name", item.name);
-            this.tags.forEach(opt => {
-              let option = document.createElement("option")
-              option.textContent = opt.split("-").join(" ");
-              option.value = opt;
-              select.appendChild(option);
-            });
-            
-            modalForm.appendChild(select);
-          } else {
-            let input = document.createElement("input");
-            input.classList.add("modal__input");
-            input.setAttribute("required", item.req);
-            input.setAttribute("type", item.type);
-            input.setAttribute("name", item.name);
-            input.placeholder = item.text;
-            modalForm.append(input);
-          }
-        });
-
-        let btnParent = document.createElement("div");
-        btnParent.classList.add("modal__btns");
-
-        let cancel = document.createElement("button");
-        cancel.classList.add("modal__btn", "modal__btn--danger");
-        cancel.textContent = "Cancel";
-        
-        let submit = document.createElement("button");
-        submit.setAttribute("type", "submit");
-        submit.classList.add("modal__btn", "modal__btn--success");
-        submit.textContent = "Create";
-
-        btnParent.append(cancel, submit);
-        modalForm.appendChild(btnParent);
-        modalbox.append(modalTitle, modalForm);
-
-        modal.appendChild(modalbox);
-        main.appendChild(modal)
-
-        var inputs = document.querySelectorAll('.modal__input');
-
-        modal.onclick = (e) => {
-          if (e.target.classList.contains("modal")) {
-            modal.classList.add("modal__hidden");
-            inputs.forEach(el => {
-              el.classList.remove("modal__input--danger")
-            });
-          }
-        }
-
-        cancel.onclick = (e) => {
-          e.preventDefault();
-          
-          modalForm.reset();
-
-          inputs.forEach(el => {
-            el.classList.remove("modal__input--danger")
-          });
-          
-          setTimeout(() => {
-            modal.classList.add("modal__hidden");
-          }, 100);
-        };
-
-        submit.onclick = (e) => {
-          e.preventDefault();
-          
-          let data = {};
-          let form = new FormData(modalForm);
-
-          form.forEach((value, name) => {
-            data.id = Date.now();
-            data[name] = value;
-            data.status = false;
-            data.createdTime = new Date();
-          });
-          
-          let inp = 0;
-          inputs.forEach(el => {
-            if (!el.value.trim()) {
-              el.classList.add("modal__input--danger")
-            } else {
-              inp++;
-              el.classList.remove("modal__input--danger")
-            }
-          });
-
-          if (inp == 3) {
-            modalForm.reset();
-            inputs.forEach(el => {
-              el.classList.remove("modal__input--danger")
-            });
-            modal.classList.add("modal__hidden");
-
-            const storedData = localStorage.getItem("data");
-
-            if (storedData) {
-              this.data = [ ...JSON.parse(storedData), { ...data } ];
-            } else {
-              this.data = [ { ...data } ];
-            }
-            localStorage.setItem("data", JSON.stringify(this.data));
-
-            const { hash } = window.location;
-            const currentDate = convertDate(new Date());
-
-            const filteredData = this.data.filter(item => {
-              switch (hash) {
-                case "#all":
-                  return true;
-                case "#today":
-                  return convertDate(item.deadline) === currentDate;
-                default:
-                  return item.tag === hash.slice(1);
-              }
-            });
-
-            setTimeout(() => {
-              this.createMenu();
-              this.render(filteredData);
-            }, 100);
-          }
-        }
-      }
-    }
+      this.createModal("new");
+    };
   }
+  // --------------------
 
+  // Filter stored data
   getStoredData(hash, data) {
-      if (data.length) {
+    let title = document.querySelector(".wrap__title");
+    title.textContent = hash.slice(1).split("-").join(" ");
+
+    if (data.length) {
+      if (this.tags.includes(hash.slice(1))) {
+        this.renderStoredData(data.filter(item => item.tag == hash.slice(1)));
+      } else {
         switch (hash) {
           case "#today":
-            this.render(data.filter(s => convertDate(s.deadline) == convertDate(new Date())));
-            break;
-          case "#daily-routine":
-            this.render(data.filter(s => s.tag == "daily-routine"));
-            break;
-          case "#study":
-            this.render(data.filter(s => s.tag == "study"));
-            break;
-          case "#more":
-            this.render(data.filter(s => s.tag == "more"));
+            this.renderStoredData(data.filter(item => convertDate(item.deadline) == convertDate(new Date())));
             break;
           case "#all":
-            this.render(data);
+            this.renderStoredData(data);
             break;
         }
-      } else {
-          const list = document.querySelector(".wrap__inner");
-          let empty = document.createElement("div");
-          empty.classList.add("wrap__empty");
-
-          const emptyText = document.createElement("h4");
-          emptyText.textContent = "Nothing here yet...";
-
-          const emptyImg = document.createElement("img");
-          emptyImg.setAttribute("src", "./imgs/empty.jpg");
-
-          empty.append(emptyImg, emptyText);
-          list.innerHTML = "";
-          list.appendChild(empty);
       }
-  }
+    } else {
+      const list = document.querySelector(".wrap__inner");
+      let empty = document.createElement("div");
+      empty.classList.add("wrap__empty");
 
-  render(data) {
+      const emptyText = document.createElement("h4");
+      emptyText.textContent = "Nothing here yet...";
+
+      const emptyImg = document.createElement("img");
+      emptyImg.setAttribute("src", "./imgs/empty.jpg");
+
+      empty.append(emptyImg, emptyText);
+      list.innerHTML = "";
+      list.appendChild(empty);
+    }
+  }
+  // --------------------------
+
+  // Render stored data in page
+  renderStoredData(data) {
     const wrap = document.querySelector(".wrap__inner");
     wrap.innerHTML = "";
 
@@ -295,12 +364,13 @@ class Todo {
 
         inp.onchange = (e) => {
           this.data = [ 
-            ...JSON.parse(localStorage.getItem("data")).map(unq => {
-              if (unq.id != item.id) return unq;
-              return { ...unq, status: !unq.status }
+            ...JSON.parse(localStorage.getItem("data")).map(i => {
+              if (i.id != item.id) return i;
+              return { ...i, status: !i.status }
             })
           ];
           localStorage.setItem("data", JSON.stringify(this.data));
+          this.notifMessage("Status changed");
         }
 
         let lb = document.createElement("label");
@@ -322,8 +392,7 @@ class Todo {
           links.forEach(link => {
             link.classList.toggle("sidebar__link--active", link.getAttribute("href") == `#${item.tag}`)
           });
-
-          this.render(JSON.parse(localStorage.getItem("data")).filter(f => f.tag == item.tag));
+          this.renderStoredData(JSON.parse(localStorage.getItem("data")).filter(f => f.tag == item.tag));
         }
 
         let del = document.createElement("button");
@@ -337,17 +406,23 @@ class Todo {
             this.getStoredData(window.location.hash, this.data);
           }, 100);
           this.createMenu();
+          this.notifMessage("Deleted successfully")
         }
 
         let edit = document.createElement("button");
         edit.title = "Edit";
         edit.innerHTML = `<img src='./imgs/edit.png'>`
 
+        edit.onclick = () => {
+          this.createModal("edit", item.id);
+        }
+
         el.append(inp, lb, sp, p, a, del, edit);
         list.appendChild(el);
       });
       
-      wrap.appendChild(list)
+      wrap.appendChild(list);
     }
   }
+  // -----------------
 }
